78af88dabf4b3de28fab2628d2654804cff38501
victorkosmo <kosymov.victor@yandex.ru>
2025-09-20 15:32:34 +0300

    fix

--- app/routes/tmaRoutes/users/index.js
+++ app/routes/tmaRoutes/users/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authenticateJWT = require('@middleware/authenticateJWT');
+const { authenticateJWT } = require('@middleware/authenticateJWT');
 const getCurrentUser = require('./me');
 
 router.get('/me', authenticateJWT, getCurrentUser);
