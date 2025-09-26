6d89186e78b9ad61b732268418a3ffa0e5fd6794
victorkosmo <kosymov.victor@yandex.ru>
2025-09-26 09:33:51 +0300

    feture setup

---
+++ app/features/sendTelegramMsg/index.js
@@ -0,0 +1,8 @@
+/*curl -X POST «BOT_URL/send-message" \
+  -H "Content-Type: application/json" \
+  -H "x-api-key: API_KEY \
+  -d '{
+    "chat_id": «user».tg_id,
+    "message": «messge_content»
+  }'
+*/
